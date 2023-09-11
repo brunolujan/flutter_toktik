038e0df9203ddb51a7fa48fdf2c5ada1e7231fcb
Bruno Luján <bruno1529@live.com.mx>
2023-09-11 13:19:37 -0600

    Likes and Views buttons implementation

--- lib/presentation/widgets/shared/video_sctrollable_view.dart
+++ lib/presentation/widgets/shared/video_sctrollable_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toktik_app/domain/entities/video_post.dart';
+import 'package:toktik_app/presentation/widgets/shared/video_buttons.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -11,17 +12,22 @@ class VideoScrollableView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
-    return PageView(
+    return PageView.builder(
       physics: const BouncingScrollPhysics(),
       scrollDirection: Axis.vertical,
-      children: [
-        Container(color: Colors.red),
-        Container(color: Colors.blue),
-        Container(color: Colors.teal),
-        Container(color: Colors.yellow),
-        Container(color: Colors.pink),
-        Container(color: Colors.deepPurple)
-      ],
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+        return Stack(
+          children: [
+          Positioned(
+           bottom: 40,
+           right: 10,
+            child: VideoButtons(video: videoPost
+            ))
+          ],
+        );
+      },
     );
   }
 }
