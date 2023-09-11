a15366302de3632d780d0a17dd4440d066be0998
Bruno Luján <bruno1529@live.com.mx>
2023-09-11 17:50:23 -0600

    video_player and fullscreen implementation with FutureBuilder

--- lib/presentation/widgets/shared/video_sctrollable_view.dart
+++ lib/presentation/widgets/shared/video_sctrollable_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toktik_app/domain/entities/video_post.dart';
 import 'package:toktik_app/presentation/widgets/shared/video_buttons.dart';
+import 'package:toktik_app/presentation/widgets/video/fullscreen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -20,11 +21,14 @@ class VideoScrollableView extends StatelessWidget {
         final VideoPost videoPost = videos[index];
         return Stack(
           children: [
-          Positioned(
-           bottom: 20,
-           right: 10,
-            child: VideoButtons(video: videoPost
-            ))
+            SizedBox.expand(
+              child: FullScreenPlayer(videoUrl: videoPost.videoUrl, caption: videoPost.caption),
+            ),
+            Positioned(
+            bottom: 20,
+            right: 10,
+              child: VideoButtons(video: videoPost)
+            )
           ],
         );
       },
