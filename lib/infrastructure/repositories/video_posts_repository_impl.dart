ec7eb7b4cb71a5c50d29d627da0842d57df5d728
Bruno Luján <bruno1529@live.com.mx>
2023-09-12 13:00:29 -0600

    Datasources and Repositories implementation

---
+++ lib/infrastructure/repositories/video_posts_repository_impl.dart
@@ -0,0 +1,21 @@
+import 'package:toktik_app/domain/datasources/video_posts_datasource.dart';
+import 'package:toktik_app/domain/entities/video_post.dart';
+import 'package:toktik_app/domain/repositories/video_posts_repository.dart';
+
+class VideoPostsRepository implements VideoPostRepository {
+  final VideoPostDataSource videoDataSource;
+
+  VideoPostsRepository({required this.videoDataSource});
+
+
+  @override
+  Future<List<VideoPost>> getFavouriteVideosByUser(String userID) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videoDataSource.getTrendingVideosByPage(page);
+  }
+
+}
